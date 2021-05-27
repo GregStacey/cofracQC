54a25f2f285925d527d71491ca9d3374a53a64a9
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2021-05-27 15:46:09 -0700

    Add error handling

--- R/measure_all_datasets.R
+++ R/measure_all_datasets.R
@@ -21,39 +21,51 @@ for (ii in 1:nrow(exp)) {
   chroms = get.chroms(exp$fn[ii])
   colnames(chroms) = paste0("fraction", 1:ncol(chroms))
   
-  # IDs and quantification
-  exp$Nid[ii] = nrow(chroms)
-  i.well.quantified = which(rowSums(!is.na(chroms))>=5)
-  exp$Nquant[ii] = length(i.well.quantified)
+  exp0 = tryCatch({
+    # IDs and quantification
+    exp$Nid[ii] = nrow(chroms)
+    i.well.quantified = which(rowSums(!is.na(chroms))>=5)
+    exp$Nquant[ii] = length(i.well.quantified)
+    
+    
+    # modern
+    autocor = calculate_autocorrelation(chroms, 10, "pearson", 1)
+    zz = calculate_z_scores(autocor, bins = 1)
+    exp$Noutliers[ii] = sum(abs(zz) > 2)
+    chroms.modern = chroms
+    chroms.modern[abs(zz)>5] = NA
+    
+    
+    # gaussian parameters
+    gauss = build_gaussians(chroms.modern, max_gaussians = 3)
+    exp$mean.ngauss[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) x$n_gaussians)))
+    exp$mean.r2[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) x$R2)))
+    exp$mean.sigma.maxA[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) {
+      unlist(x$coefs$sigma[which.max(x$coefs$A)])
+    })))
+    exp$sd.sigma.maxA[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) {
+      unlist(x$coefs$sigma[which.max(x$coefs$A)])
+    })))
+    
+    # corum overlap
+    exp$N.corum.overlap[ii] = sum(rownames(chroms) %in% corum.prots)
+    adj = corum.edges %>% dplyr::select(1:2) %>% 
+      filter(.[[1]]%in%rownames(chroms) & .[[2]]%in%rownames(chroms)) %>%
+      adjacency_matrix_from_data_frame()
+    dd = 1 - amap::Dist(chroms, method = "pearson") %>% as.matrix()
+    dd = dd[match(rownames(adj), rownames(dd)), match(rownames(adj), rownames(dd))]
+    exp$corum.corr[ii] = mean(dd[adj==1])
+    
+    return(exp)
+  },
+  error=function(cond) {
+    message(paste0("Failed to do QC for ", exp$fn[ii]))
+    message("original error message:")
+    message(cond)
+    return(NA)
+  })
   
-  
-  # modern
-  autocor = calculate_autocorrelation(chroms, 10, "pearson", 1)
-  zz = calculate_z_scores(autocor, bins = 1)
-  exp$Noutliers[ii] = sum(abs(zz) > 2)
-  chroms.modern = chroms
-  chroms.modern[abs(zz)>5] = NA
-  
-  
-  # gaussian parameters
-  gauss = build_gaussians(chroms.modern, max_gaussians = 3)
-  exp$mean.ngauss[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) x$n_gaussians)))
-  exp$mean.r2[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) x$R2)))
-  exp$mean.sigma.maxA[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) {
-    unlist(x$coefs$sigma[which.max(x$coefs$A)])
-  })))
-  exp$sd.sigma.maxA[ii] = mean(unlist(sapply(gauss[i.well.quantified], function(x) {
-    unlist(x$coefs$sigma[which.max(x$coefs$A)])
-  })))
-  
-  # corum overlap
-  exp$N.corum.overlap[ii] = sum(rownames(chroms) %in% corum.prots)
-  adj = corum.edges %>% dplyr::select(1:2) %>% 
-    filter(.[[1]]%in%rownames(chroms) & .[[2]]%in%rownames(chroms)) %>%
-    adjacency_matrix_from_data_frame()
-  dd = 1 - amap::Dist(chroms, method = "pearson") %>% as.matrix()
-  dd = dd[match(rownames(adj), rownames(dd)), match(rownames(adj), rownames(dd))]
-  exp$corum.corr[ii] = mean(dd[adj==1])
+  if (!is.na(exp0)) exp = exp0
   
   # write
   write_csv(exp, file = "data/files_cofracQC.csv")
